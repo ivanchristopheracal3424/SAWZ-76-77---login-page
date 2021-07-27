44a2682f5d0b6bd59c03c75c181b5d0b01bb224d
Ivan Christopher Acal <aarah3424@gmail.com>
2021-07-27 19:38:39 +0800

    login page -- issue in console

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgControl, Validators } from '@angular/forms';
+import { AuthServiceService } from '../auth-service.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+export class LoginComponent implements OnInit {
+  formGroup!:FormGroup;
+
+  // formGroup = new FormGroup ({
+  //   email: new FormControl('',[Validators.required]),
+  //   password: new FormControl('',[Validators.required])
+  // });
+
+  constructor(private authService:AuthServiceService) {}
+  ngOnInit() {
+    this.initForm();
+  }
+
+
+  initForm() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required])
+    })
+  }
+
+
+  loginProccess(){
+    if(this.formGroup.valid){
+      this.authService.login(this.formGroup.value).subscribe(result=>{
+        if(result.success){
+          console.log(result);
+          alert(result.message);
+        }
+        else {
+          alert(result.message);
+        }
+      })
+    }
+  }
+
+}
